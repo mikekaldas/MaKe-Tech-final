77be16a3a4d7784620c3359ee36991035df6d10c
mikekaldas <mikekaldas@gmail.com>
2026-03-25 21:40:48 -0400

    Replace LandingPage with Hello World message

--- App.jsx
+++ App.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import LandingPage from './LandingPage';
-
 export default function App() {
-  return <LandingPage />;
+  return <div style={{ background: 'red', width: '100vw', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white', fontSize: '40px' }}>HELLO WORLD</div>;
 }
